c0df36d724b0dcc189c7ea05f9146633bc60b245
懒得勤快 <chenyu0@outlook.com>
2017-09-16 11:39:45 +0800

    2.17.9.15更新

--- GoAgent/update2.bat
+++
@@ -1,7 +0,0 @@
-@Echo Off
-Title 从OSC云端更新 Agent 最新可用 IP
-cd /d %~dp0
-del "..\Agent\proxy.user.ini_backup"
-ren "..\Agent\proxy.user.ini"  proxy.user.ini_backup
-wget --ca-certificate=ca-bundle.crt -c https://coding.net/u/Alvin9999/p/ip/git/raw/master/proxy.user.ini
-ECHO.&ECHO.已更新完成最新可用IP,请按任意键退出程序. &PAUSE >NUL 2>NUL
